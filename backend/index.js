176b7dda654299edebf9a9fe39a64cca7479c83e
PJ <joppy007@gmail.com>
2025-02-02 14:02:25 -0500

    install middleware and setup mongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(cors());
+app.use(express.urlencoded( { extended: true } ));
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.json("Welcome to PJ_Capstone");
 });
